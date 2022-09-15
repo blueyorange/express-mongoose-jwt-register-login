a7856aa036572e7c303bf60198075c775d724061
Russell <blueyorange@gmail.com>
2022-09-15 11:31:41 +0100

    fixed issue with regex patterns

--- routes/register.routes.js
+++ routes/register.routes.js
@@ -3,18 +3,13 @@ const router = express.Router();
 const User = require("../models/user.model.js");
 
 router.get("/", async (req, res) => {
-  console.log(User.schema.obj.username.match);
-  const patterns = Object.keys(User.schema.obj).map((key) => {
-    const pattern = User.schema.obj[key];
-    return { key, pattern };
-  });
-  console.log(patterns);
   return res.render("register.njk", { schema: User.schema.obj });
 });
 
 router.post("/", async (req, res, next) => {
   const { firstname, surname, dateOfBirth, username, email, password } =
     req.body;
+  console.log(dateOfBirth);
   const user = new User({
     firstname,
     surname,
